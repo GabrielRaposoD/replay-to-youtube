11732b978033f2b257d67dd1cb3052fa9e437e03
Gabriel Raposo <gabriel.robertor@gmail.com>
2022-07-19 18:53:41 -0300

    adds thumbnail creator

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from entities.match_data import MatchData
 from entities.data_scrapper import DataScrapper
 from utils.data import load_match_data
 from utils.scrapper import Scrapper
+from utils.thumbnail_creator import CreateThumbnail
 
 scrapper = Scrapper()
 scrapper.init_data_folder()
 scrapper.start_scrap()
 match_data: MatchData = load_match_data()
+thumb_creator = CreateThumbnail(data_scrapper=DataScrapper(), data=match_data)
+thumb_creator.create_thumbnail()
